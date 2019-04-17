187c6af617867ce67c23c548654a9c864cfcaa89
grubbyhunter <grubbyhunter@gmail.com>
2019-04-17 19:36:08 +0800

    提交三数之和

---
+++ 15.三数之和.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=javascript
+ *
+ * [15] 三数之和
+ *
+ * https://leetcode-cn.com/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (21.82%)
+ * Total Accepted:    48.3K
+ * Total Submissions: 221.5K
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0
+ * ？找出所有满足条件且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ *
+ * 满足要求的三元组集合为：
+ * [
+ * ⁠ [-1, 0, 1],
+ * ⁠ [-1, -1, 2]
+ * ]
+ *
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  let length = nums.length
+  // 首先给数组排序，按照升序排列
+  for(let i = length -1;)
+}
