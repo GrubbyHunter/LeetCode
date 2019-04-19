b78810666427490deb8ad869dd7dda4b77278d6f
grubbyhunter <grubbyhunter@gmail.com>
2019-04-19 21:49:33 +0800

    提交搜索旋转数组

---
+++ 33.搜索旋转排序数组.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ *
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (36.28%)
+ * Total Accepted:    21.4K
+ * Total Submissions: 59K
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ *
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ *
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ *
+ * 你可以假设数组中不存在重复的元素。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let left = 0,
+    right = nums.length - 1
+
+  while (left < right) {
+    let middle = parseInt((left + right) / 2)
+
+    if (nums[middle] == target) {
+      return middle
+    }
+
+    // 在右边
+    if (nums[middle] > target) {
+      if (nums[middle] > nums[right]) {
+        left = nums[middle]
+      }
+
+      return
+    }
+
+    // 在左边
+  }
+
+  return -1
+}
