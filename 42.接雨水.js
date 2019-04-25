00a4eb00daafe9f85b39835f477f349f3571c098
grubbyhunter <grubbyhunter@gmail.com>
2019-04-25 17:44:15 +0800

    提交新题目

--- 42.接雨水.js
+++ 42.接雨水.js
@@ -8,13 +8,37 @@
  * @return {number}
  */
 var trap = function(height) {
-  let size = 0
   let max = 0
   let key = 0
-  for (var i = 1; i < height.length; i++) {
+
+  for (var i = 0; i < height.length; i++) {
     if (max < height[i]) {
       max = height[i]
       key = i
+    }
+  }
+
+  let left = 0
+  let right = height.length - 1
+  let sum = 0
+  debugger
+  for (let i = 0; i < key; i++) {
+    if (height[left] > height[i]) {
+      sum = sum + height[left] - height[i]
+    }
+
+    if (height[left] < height[i]) {
+      left = i
+    }
+  }
+
+  for (let i = right; i > key; i--) {
+    if (height[right] > height[i]) {
+      sum = sum + height[right] - height[i]
+    }
+
+    if (height[right] < height[i]) {
+      right = i
     }
   }
 
