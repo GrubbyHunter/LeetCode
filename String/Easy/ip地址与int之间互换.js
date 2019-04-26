da440ad1297a33e7f73b41f14bde35bd0aa4f5f2
grubbyhunter <grubbyhunter@gmail.com>
2019-04-26 23:24:08 +0800

    新增题目：ip与数字之间的转换

---
+++ String/Easy/ip地址与int之间互换.js
@@ -0,0 +1,15 @@
+/**
+ *将一个字符串ip转换成一个数字
+ *
+ * @param {string} ip
+ * @return {number}
+ */
+function ipToNumber(ip) {}
+
+/**
+ *将一个数字转换成一个字符串ip地址
+ *
+ * @param {number} num
+ * @return {string} ip
+ */
+function numberToIp(num) {}
