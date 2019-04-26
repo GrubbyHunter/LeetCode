cb116c3df19dcc47cef6ec855ca94eb9bc048e2e
grubbyhunter <grubbyhunter@gmail.com>
2019-04-26 23:46:18 +0800

    格式化

--- String/Easy/ip地址与int之间互换.js
+++ String/Easy/ip地址与int之间互换.js
@@ -7,11 +7,14 @@
 function ipToNumber(ip) {
   let array = ip.split(".")
   let str = ""
+
   for (let i = 0; i < array.length; i++) {
     // 普通数字转二进制然后进行累加字符串长度
     let temp = parseInt(array[i]).toString(2)
+
     if (temp.length < 8) {
       let count = 8 - temp.length
+
       while (count > 0) {
         temp = "0" + temp
         count--
@@ -21,7 +24,6 @@ function ipToNumber(ip) {
     str += temp
   }
 
-  // 二进制转十进制
   return parseInt(str, 2)
 }
 
@@ -35,6 +37,7 @@ function numberToIp(num) {
   let str = num.toString(2)
   let i = 0
   let ip = ""
+
   while (i < 32) {
     // 二进制转十进制
     ip = ip + "." + parseInt(str.substr(i, 8), 2)
