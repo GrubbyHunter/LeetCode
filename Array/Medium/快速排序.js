decd074fdfce81607e200a495cc517f2ff075d3b
grubbyhunter <grubbyhunter@gmail.com>
2019-05-05 20:51:26 +0800

    提交快速排序

---
+++ Array/Medium/快速排序.js
@@ -0,0 +1,51 @@
+let array = [1, 6, 4, 7, 5, 9, 10, 4, 19, 2, 11, 8]
+let quickSort = array => {
+  let length = array.length
+
+  let sort = arr => {
+    if (arr.length == 1) {
+      return arr
+    }
+
+    let l = 0,
+      r = arr.length - 1
+
+    if (l >= r) {
+      return arr
+    }
+
+    let item = arr[0]
+    let temp
+
+    while (l < r) {
+      //从右边开始找，找到比基准小的元素
+      while (arr[r] >= item && r > l) {
+        r--
+      }
+
+      //从左边开始找，找到比基准打的元素
+      while (arr[l] <= item && r > l) {
+        l++
+      }
+
+      // 然後互换，重复这个过程
+      temp = arr[l]
+      arr[l] = arr[r]
+      arr[r] = temp
+    }
+
+    //
+    temp = arr[0]
+    arr[0] = arr[r]
+    arr[r] = temp
+
+    let left = sort(arr.slice(0, r))
+    let right = sort(arr.slice(r + 1, length))
+
+    return left.concat(item).concat(right)
+  }
+
+  return sort(array)
+}
+
+quickSort(array)
