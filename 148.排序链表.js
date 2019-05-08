f6177426d9805d450831bced936b28a5003407f9
grubbyhunter <grubbyhunter@gmail.com>
2019-05-08 23:32:37 +0800

    快速排序链表

---
+++ 148.排序链表.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=javascript
+ *
+ * [148] 排序链表
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+  if (!head || !head.next) {
+    return head
+  }
+  let left = head.next
+  let right = head.next
+  let item = head.next
+  while (item) {}
+}
