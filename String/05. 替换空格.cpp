1943586e2f952937694cb33bcbaaf316436cb6a0
karsashen <karsashen@tencent.com>
2021-04-08 20:59:36 +0800

    feature add；调整代码

--- String/05. 替换空格.cpp
+++ String/05. 替换空格.cpp
@@ -19,8 +19,8 @@ public:
   string replaceSpace(string s)
   {
     int count = 0;
-
-    for (int i = 0; i < s.size(); i++)
+    int size = s.size();
+    for (int i = 0; i < size; i++)
     {
       if (s[i] == ' ')
       {
@@ -30,14 +30,17 @@ public:
 
     // 计算替换完%20之后数组的长度，这里每个空格变成了%20
     // 相当于每个空格就要多出2个元素的长度
-    int maxSize = s.size() + count * 2;
+    int maxSize = size + count * 2;
     s.resize(maxSize);
+    // 双指针从数组尾部向头部依次替换，保证不会动到前面的元素
+    // 如果头部向尾部进行替换，每改一次都需要动到后面元素的位置
+    int startIndex = maxSize - 1; // 填充后数组的最后一个元素作为慢指针
+    int endIndex = size - 1;      // 原始数组最后一个元素最为快指针
 
-    int startIndex = maxSize - 1;
-    int endIndex = s.size() - 1;
-
-    while (startIndex >= 2 && endIndex >= 0)
+    // 一直替换到第一个元素，因为startIndex -2 >= 0
+    while (startIndex >= 3 && endIndex >= 0)
     {
+      // 非空格的话，元素直接移动到后面
       if (s[endIndex] != ' ')
       {
         s[startIndex] = s[endIndex];
@@ -46,9 +49,12 @@ public:
       }
       else
       {
+        // 空格的话直接填充最近的三个位置，然后慢指针移动到第四个位置，同时快指针移动到下一个位置
         s[startIndex] = '0';
         s[startIndex - 1] = '2';
         s[startIndex - 2] = '%';
+        startIndex -= 3;
+        endIndex--;
       }
     }
 
