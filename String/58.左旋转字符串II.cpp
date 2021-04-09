a7194dd94d0543d0a75feaf2baacfc5a117883a4
karsashen <karsashen@tencent.com>
2021-04-09 14:41:13 +0800

    feature add；左旋字符串2

--- String/58.左旋转字符串II.cpp
+++ String/58.左旋转字符串II.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <string>
-
+// 命名空间，有命名空间才能使用std下的全局变量
 using namespace std;
+
+/*
+* 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。
+* 比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+* 
+* 示例 1：
+* 输入: s = "abcdefg", k = 2  gfedcba
+* 输出: "cdefgab"
+* 
+* 示例 2：
+* 输入: s = "lrloseumgh", k = 6
+* 输出: "umghlrlose"
+* 限制：
+* 1 <= k < s.length <= 10000
+*/
+
 class Solution
 {
 public:
@@ -33,18 +49,3 @@ public:
   }
 };
 // @lc code=end
-
-int main()
-{
-  // vector<int> result;
-  // result.push_back(2);
-  // result.push_back(3);
-  // result.push_back(1);
-  // result.push_back(2);
-  // result.push_back(4);
-  // result.push_back(3);
-  Solution so;
-  string str1 = "abcdefg";
-  so.reverseLeftWords(str1, 2);
-  return 0;
-}
