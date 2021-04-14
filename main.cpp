fed3b823a51cfa004b8bee1068f0501b4cd9a1c5
karsashen <karsashen@tencent.com>
2021-04-14 12:54:59 +0800

    feature add: 三数之和

--- main.cpp
+++ main.cpp
@@ -18,31 +18,77 @@ struct ListNode
 class Solution
 {
 public:
-  bool canConstruct(string ransomNote, string magazine)
+  vector<vector<int>> threeSum(vector<int> &nums)
   {
-    unordered_map<char, int> ransomMap;
-    for (char w : ransomNote)
+    vector<vector<int>> result;
+    // 先对数组进行快速排序
+    for (int i = 0; i < nums.size(); i++)
     {
-      ransomMap[w]++;
-    }
-
-    for (char w : magazine)
-    {
-      if (ransomMap.find(w) != ransomMap.end())
+      for (int j = i; j < nums.size(); j++)
       {
-        ransomMap[w]--;
+        if (nums[i] > nums[j])
+        {
+          int temp = nums[i];
+          nums[i] = nums[j];
+          nums[j] = temp;
+        }
       }
     }
 
-    for (auto iter = ransomMap.begin(); iter != map.end(); ++iter)
+    // nums[i] + nums[left] + nums[right] = 0
+    for (int i = 0; i < nums.size(); i++)
     {
-      if (iter->second > 0)
+      // 第一个数大于0，则后面的数加起来更不可能 == 0，直接返回结果
+      if (nums[i] > 0)
       {
-        return false;
+        return result;
+      }
+
+      // 正确去重方法
+      if (i > 0 && nums[i] == nums[i - 1])
+      {
+        continue;
+      }
+
+      int left = i + 1;
+      int right = nums.size() - 1;
+
+      while (left < right)
+      {
+        // 首先对指针的两边进行去重，比如0022，实际上找到的是中间的02作为左右指针的起始位置
+        while (right > left && nums[right] == nums[right - 1])
+          right--;
+        while (right > left && nums[left] == nums[left + 1])
+          left++;
+
+        if (nums[i] + nums[left] + nums[right] == 0)
+        {
+          // 符合条件
+          result.push_back({nums[i], nums[left], nums[right]});
+
+          // 找到之后继续对双指针去重
+          while (right > left && nums[right] == nums[right - 1])
+            right--;
+          while (right > left && nums[left] == nums[left + 1])
+            left++;
+          // 左右各自往中间走一步
+          left++;
+          right--;
+        }
+        else if (nums[i] + nums[left] + nums[right] < 0)
+        {
+          // sum<0，移动左边
+          left++;
+        }
+        else
+        {
+          // sum>0，移动右边
+          right--;
+        }
       }
     }
 
-    return true;
+    return result;
   }
 };
 // @lc code=end
@@ -55,11 +101,8 @@ int main()
   //ListNode *head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
   ListNode *head = new ListNode(1);
   Solution so;
-  vector<int> nums;
-  nums.push_back(2);
-  nums.push_back(3);
-  nums.push_back(4);
-  nums.push_back(7);
-  so.twoSum(nums, 9);
+  vector<int> nums = {-2, 0, 0, 2, 2};
+
+  so.threeSum(nums);
   return 0;
 }
