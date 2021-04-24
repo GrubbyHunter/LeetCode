291853307f6f27e909c838ac13141341ef4d21e4
karsashen <karsashen@tencent.com>
2021-04-24 20:26:32 +0800

    feature add:Binary Tree/101.对称二叉树.cpp

--- main.cpp
+++ main.cpp
@@ -38,28 +38,32 @@ struct TreeNode
 class Solution
 {
 public:
-  TreeNode *invertTree(TreeNode *root)
+  bool compare(TreeNode *left, TreeNode *right)
   {
-    queue<TreeNode *> qu;
-    if (root != nullptr)
+    if (left == nullptr && right == nullptr)
     {
-      qu.push(root);
+      return true;
     }
 
-    while (!qu.empty())
+    if (left != nullptr && right != nullptr)
     {
-      TreeNode *cur = qu.front();
-      qu.pop();
-
-      TreeNode *temp = cur->left;
-      cur->left = cur->right;
-      cur->right = temp;
-
-      qu.push(cur->left);
-      qu.push(cur->right);
+      if (left->val != right->val)
+      {
+        return false;
+      }
+      return compare(left->left, right->right) && compare(left->right, right->left);
     }
 
-    return root;
+    return false;
+  }
+  bool isSymmetric(TreeNode *root)
+  {
+    if (root == nullptr)
+    {
+      return true;
+    }
+
+    return compare(root->left, root->right);
   }
 };
 // @lc code=end
