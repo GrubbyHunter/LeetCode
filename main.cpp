247eac42c6e5557b83f1b9316c96785128e036c0
karsashen <karsashen@tencent.com>
2021-04-26 14:49:56 +0800

    featuren add:Binary Tree/222.完全二叉树的节点个数.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <queue>
 #include <unordered_set>
 #include <unordered_map>
+#include <math.h>
 
 using namespace std;
 
@@ -40,31 +41,37 @@ class Solution
 public:
   int getLevel(TreeNode *root)
   {
+    int level = 0;
+
+    // 完全二叉树，只需要计算左子树就能确定层级
+    while (root != nullptr)
+    {
+      root = root->left;
+      level++;
+    }
+
+    return level;
+  }
+  int countNodes(TreeNode *root)
+  {
     if (root == nullptr)
     {
       return 0;
     }
-    int leftDepth = getLevel(root->left);   // 左
-    int rightDepth = getLevel(root->right); // 右
-                                            // 中
-    // 当一个左子树为空，右不为空，这时并不是最低点
-    if (root->left == NULL && root->right != NULL)
-    {
-      return 1 + rightDepth;
-    }
-    // 当一个右子树为空，左不为空，这时并不是最低点
-    if (root->left != NULL && root->right == NULL)
-    {
-      return 1 + leftDepth;
-    }
 
-    int result = 1 + min(leftDepth, rightDepth);
-    return result;
-  }
+    int leftLevel = getLevel(root->left);
+    int rightLevel = getLevel(root->right);
 
-  int minDepth(TreeNode *root)
-  {
-    return getLevel(root);
+    // 左右层级相等，那么最后一个节点在右边，左边为满树，不然只能右侧为满树
+    if (leftLevel == rightLevel)
+    {
+      // <<是位运算符号，代表把1的二进制表示左移n位
+      // 左移一位（即在原来的数后面加一个0）相当于乘以2，左移n位应该是相当于乘以2的n次方
+      // 左边满树 + 中间顶点 + 右边子树数量
+      return ((1 << leftLevel) - 1) + 1 + countNodes(root->right);
+    }
+    // 右边满树 + 中间顶点 + 左边子树数量
+    return ((1 << rightLevel) - 1) + 1 + countNodes(root->left);
   }
 };
 // @lc code=end
@@ -72,7 +79,7 @@ public:
 int main()
 {
   // new 对象返回的是地址的引用，就是一个指针
-  TreeNode *head = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+  TreeNode *head = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6), nullptr));
   //ListNode *head;
   //ListNode *head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
   Solution so;
@@ -81,6 +88,6 @@ int main()
   //st.push(1);
   //st.push(2);
   //st.top();
-  so.minDepth(head);
+  int count = so.countNodes(head);
   return 0;
 }
