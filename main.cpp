2f2fd03c7dd8787889a4210825b2b7562c42fe1e
karsashen <karsashen@tencent.com>
2021-05-16 16:07:46 +0800

    feature add：Back Tracking/组合/77.组合.cpp

--- main.cpp
+++ main.cpp
@@ -40,20 +40,37 @@ struct TreeNode
 class Solution
 {
 public:
-  int sum;
-  TreeNode *convertBST(TreeNode *root)
+  vector<vector<int>> result;
+  vector<int> path;
+  void findPath(int n, int k, int startIndex)
   {
-    // 使用反向中序遍历，也就是右->中->左的形式
-    if (root == nullptr)
+    // 数组中的元素数量达到k个，则满足条件，递归停止，并将结果添加到最后的结果集
+    if (path.size() == k)
     {
-      return nullptr;
+      result.push_back(path);
+      return;
     }
 
-    root->right = convertBST(root->right); // right
-    sum += root->val;                      //累计计算sum
-    root->val = sum;                       // middle
-    root->left = convertBST(root->left);   //left
-    return root;
+    // 这里的循环是横向遍历，将n的数组每个值一次跟后面的元素组合
+    for (int i = startIndex; i <= n; i++)
+    {
+      // 第i个元素
+      path.push_back(i);
+      // 以及它后面的元素i+1，进行组合
+      findPath(n, k, i + 1);
+      // 组合完之后进行回溯，将i推出数组，开始统计下一个元素的组合情况
+      path.pop_back();
+    }
+  }
+  vector<vector<int>> combine(int n, int k)
+  {
+    if (n == 0 || k == 0)
+    {
+      return result;
+    }
+
+    findPath(n, k, 1);
+    return result;
   }
 };
 // @lc code=end
