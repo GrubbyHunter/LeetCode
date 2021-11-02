cec4a03ba8ca41bd60f076197efe6d8cc04d9988
karsashen <karsashen@tencent.com>
2021-11-02 20:04:39 +0800

    List/707.设计链表.ts

--- List/203.移除链表元素.ts
+++ List/203.移除链表元素.ts
@@ -16,15 +16,10 @@
  *     }
  * }
  */
-// class ListNode {
-//   val: number
-//   next: ListNode | null
-//   constructor(val?: number, next?: ListNode | null) {
-//     this.val = (val === undefined ? 0 : val)
-//     this.next = (next === undefined ? null : next)
-//   }
-// }
 
+// 两种处理方式：
+// 1、使用一个虚拟指针指向链表头（head），这样就能一直用current.next这个条件进行判断
+// 2、先遍历找到链表中第一个符合条件的元素，然后遍历时候使用current这个条件进行判断
 function removeElements(head: ListNode | null, val: number): ListNode | null {
   // 初始化一个空节点指向头指针，以便循环条件 currnet.next 能够顺利进行
   const result = new ListNode(0, head)
