6269e06187e84a2357dbf1b45f448f0e26744e7c
karsashen <karsashen@tencent.com>
2021-11-10 12:35:10 +0800

    Hash/202.快乐数.ts

--- Hash/202.快乐数.ts
+++ Hash/202.快乐数.ts
@@ -1,29 +1,35 @@
 /*
- * @lc app=leetcode.cn id=35 lang=typescript
+ * @lc app=leetcode.cn id=202 lang=typescript
  *
- * [35] 搜索插入位置
+ * [202] 快乐数
  */
+
+// @lc code=start
 function isHappy(n: number): boolean {
   // sum是下一次拆分进行平方的原始数据
   // 所以这里使用Object记录sum是否出现过，出现贵的话就会陷入死循环，一定不是快乐树
-  let result: any = {}
+  let result = {}
   let sum = 0
 
+  // 存在且不止出现过一次，死循环了，肯定不是快乐数，return false
   while (!result[n] || result[n] === 1) {
+    // 求和开始
     while (n > 9) {
       let temp = n % 10
       n = Math.floor(n / 10)
       sum += Math.pow(temp, 2)
     }
     sum += Math.pow(n, 2)
-
+    // 等于1，退出循环
     if (sum === 1) {
       return true
     }
 
+    // 已经出现过一次，变成第二次
     if (result[sum] === 1) {
       result[sum] = 2
     } else {
+      // 没有出现过，记录第一次
       result[sum] = 1
     }
 
@@ -31,10 +37,7 @@ function isHappy(n: number): boolean {
     sum = 0
   }
 
-
   return false
 };
-
-isHappy(19);
 // @lc code=end
 
