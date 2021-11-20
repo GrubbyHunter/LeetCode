e142df4e1eb892dfe29c647eb722b62a27772c4f
karsashen <karsashen@tencent.com>
2021-11-20 21:02:31 +0800

    Binary Tree/二叉树的遍历方式/102.二叉树的层序遍历.ts

---
+++ Binary Tree/二叉树的遍历方式/144.二叉树的前序遍历-非递归实现.ts
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=typescript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+  let result = []
+  let stack = [root]
+
+  while (stack.length !== 0) {
+    const last = stack.pop()
+    if (!last) {
+      continue
+    }
+
+    result.push(last.val)
+
+    if (last.right) {
+      stack.push(last.right)
+    }
+
+    if (last.left) {
+      stack.push(last.left)
+    }
+  }
+
+  return result
+};
+// @lc code=end
+
