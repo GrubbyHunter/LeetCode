902cd3a2ee6163cccb06a89c0e8bc70c09f68d6f
karsashen <karsashen@tencent.com>
2021-11-22 17:03:56 +0800

    Binary Tree/二叉树的修改与构造/106.从中序与后序遍历序列构造二叉树.ts

--- index.ts
+++ index.ts
@@ -3,60 +3,53 @@
  *
  * [35] 搜索插入位置
  */
-function hasPathSum(root: any | null, targetSum: number): boolean {
-  const getSum = (sum: number, node: any | null) => {
-    if (!node) {
-      return -1
-    }
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
 
-
-    if (!node.left && !node.right) {
-      if (node.val === sum) {
-        return 0
-      } else {
-        return -1
-      }
-    }
-
-    let result = sum - node.val
-
-    let left = getSum(result, node.left)
-    if (left === 0) {
-      return 0
-    }
-
-    let right = getSum(result, node.right)
-    if (right === 0) {
-      return 0
-    }
-
-    return -1
+function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+  if (inorder.length === 0) {
+    return null
+  }
+  // 遍历完成
+  if (inorder.length === 1) {
+    return new TreeNode(inorder[0])
   }
 
-  return getSum(targetSum, root) === 0
+  // 后序遍历的最后一个元素为顶部节点
+  let middle = postorder.pop()
+  let left = null
+  let i = 0
+
+  for (; i < inorder.length - 1; i++) {
+    if (inorder[i + 1] === middle) {
+      left = inorder[i]
+      break
+    }
+  }
+
+  // 去掉midele和left节点
+  inorder.splice(i, 2)
+  // 去掉left节点
+  for (i = 0; i < postorder.length; i++) {
+    if (postorder[i] === left) {
+      break
+    }
+  }
+  postorder.splice(i, 1)
+
+  let leftNode = left ? new TreeNode(left) : null
+  return new TreeNode(middle, leftNode, buildTree(inorder, postorder))
 };
 
-const arr = hasPathSum({
-  val: 1,
-  left: {
-    val: -2,
-    left: {
-      val: 1,
-      left: {
-        val: -1
-      }
-    },
-    right: {
-      val: 3
-    }
-  },
-  right: {
-    val: -3,
-    left: {
-      val: -2
-    },
-  }
-}, -1);
+const arr = buildTree([2, 1], [2, 1])
 
 // @lc code=end
 
