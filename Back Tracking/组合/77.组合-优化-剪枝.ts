7c135fa0940ae1dd998448877c37229f8d980c89
karsashen <karsashen@tencent.com>
2021-11-30 09:37:58 +0800

    Back Tracking/组合/77.组合-优化-剪枝.ts

--- Back Tracking/组合/77.组合-优化-剪枝.ts
+++ Back Tracking/组合/77.组合-优化-剪枝.ts
@@ -1,21 +1,25 @@
 /*
- * @lc app=leetcode.cn id=35 lang=typescript
+ * @lc app=leetcode.cn id=77 lang=typescript
  *
- * [35] 搜索插入位置
+ * [77] 组合
  */
+
+// @lc code=start
 function combine(n: number, k: number): number[][] {
-  let sum: any = []
+  let sum = []
 
   const backTracking = (start: number, result: number[]): void => {
     // 终止条件数组已经达到k个元素
     if (result.length === k) {
+      // 由于数组是引用传递，这里需要构建一个新的数组来作为结果保存
       sum.push([...result])
       return
     }
-
-    for (let i = start; i <= n; i++) {
+    // 剪枝，如果剩余的数字已经不够k个元素，那么组合不满足，直接排除
+    for (let i = start; i <= n - (k - result.length) + 1; i++) {
       result.push(i)
       backTracking(++start, result)
+      // 元素用完进行回溯移除
       result.pop()
     }
   }
@@ -23,7 +27,5 @@ function combine(n: number, k: number): number[][] {
   backTracking(1, [])
   return sum
 };
-const arr = combine(4, 2)
-
 // @lc code=end
 
