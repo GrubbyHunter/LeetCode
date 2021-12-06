19326e684011cbd1f141d8fad20626f4a0aaa361
karsashen <karsashen@tencent.com>
2021-12-06 14:56:01 +0800

    Back Tracking/组合/491.递增子序列.ts

--- index.ts
+++ index.ts
@@ -3,45 +3,35 @@
  *
  * [35] 搜索插入位置
  */
-function partition(s: string): string[][] {
-  let result: any = []
-  const arrStr: any = s.split("")
-  const size = arrStr.length
+function findSubsequences(nums: number[]): number[][] {
+  const result: any = []
+  const used: any = {}
 
-  if (size === 0) {
+  if (nums.length <= 1) {
     return result
   }
-  // 判断是否是回文的函数
-  const isPalindrome = (str: string[]) => {
-    let left = 0
-    let right = str.length - 1
-    while (left < right) {
-      if (str[left] !== str[right]) {
-        return false
-      }
-      left++
-      right--
-    }
 
-    return true
-  }
-  const backTracking = (arr: string[], start: number) => {
-    // 切割到最后一个，收集结果
-    if (start === size - 2) {
+  const backTracking = (arr: number[], start: number) => {
+    // 保存之前的递增序列
+    if (arr.length >= 2) {
       result.push([...arr])
-      return
     }
 
-    for (let i = start; i < size; i++) {
-      // 切割的位置在当前元素的后面，所以要 i + 1
-      const currentArr = arrStr.slice(start, i + 1)
-      // 当前切割的字符不是回文，直接进行下一次循环
-      if (!isPalindrome(currentArr)) {
-        continue
+    for (let i = start; i < nums.length; i++) {
+
+      if (arr.length > 0) {
+        // 上一个数大于当前，跳过当前这个数
+        // 这里上一个数不是i-1，而是当前数组arr的最后一个数
+        if (arr[arr.length - 1] > nums[i]) {
+          continue
+        }
+
       }
-      // join组成字符创，放入结果数组
-      arr.push(currentArr.join(""))
+
+      arr.push(nums[i])
+      used[i] = true
       backTracking(arr, i + 1)
+      used[i] = false
       arr.pop()
     }
   }
@@ -50,7 +40,7 @@ function partition(s: string): string[][] {
 
   return result
 };
-partition("aab")
+findSubsequences([4, 6, 7, 7])
 
 // @lc code=end
 
