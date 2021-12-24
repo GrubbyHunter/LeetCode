1af6279af6b664581cdeb7aaa2f799373af9ed89
karsashen <karsashen@tencent.com>
2021-12-24 17:00:07 +0800

    add

--- index.ts
+++ index.ts
@@ -3,33 +3,35 @@
  *
  * [35] 搜索插入位置
  */
-function partitionLabels(s: string): number[] {
-  let str = s.split("")
-  let result: any = []
-  let map: any = {}
+function maxProfit(prices: number[], fee: number): number {
+  let minPrice = prices[0]
+  let currentMinPrice = prices[0]
+  let maxSum = 0
+  let sum = 0
 
-  // 使用map记录每个字符出现的最后一个下标
-  for (let i = 0; i < str.length; i++) {
-    map[str[i]] = i
-  }
+  for (let i = 0; i < prices.length; i++) {
 
-  let left = 0
-  let right = 0
-
-  for (let i = 0; i < str.length; i++) {
-    // left到i这个范围内出现的字符最远边界
-    // 不用担心范围内有更远的边界，有的话会用max取值拓宽边界
-    right = Math.max(right, map[str[i]])
-
-    // 最远边界跟当前下标相等，则为分割点
-    if (i === right) {
-      result.push(right - left + 1)
-      left = i + 1
+    if (i === 0) {
+      continue
     }
+
+    // 计算之交易一次时候能得到的最大值
+    let currentSum = prices[i] - minPrice
+
+    if (currentSum > 0) {
+      maxSum = Math.max(currentSum, maxSum)
+
+    } else {
+      minPrice = prices[i]
+      currentMinPrice = prices[i]
+    }
+
+    // 计算交易多次时候的累计最大值
+
   }
 
-  return result
+  return Math.max(sum, maxSum - fee)
 };
-partitionLabels("ababcbacadefegdehijhklij")
+maxProfit([1, 4, 6, 2, 8, 3, 10, 14], 3)
 // @lc code=end
 
