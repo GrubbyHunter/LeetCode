4fbc40626d6c914c047413e8f3ba9c00bc5fdca1
karsashen <karsashen@tencent.com>
2021-12-27 09:56:24 +0800

    Dynamic programming/easy/70.爬楼梯.ts

---
+++ Dynamic programming/easy/70.爬楼梯.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=typescript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+  let dp = [1, 2]
+
+  // 这里不使用递归，直接用数组，减少内存占用
+  for (let i = 2; i < n; i++) {
+    // i-1再走一步到达i
+    // i-2 再走两步到达i
+    // 所以到第i层有dp[i - 1] + dp[i - 2]种方法
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+
+  // n為下标，所以要-1
+  return dp[n - 1]
+};
+// @lc code=end
+
