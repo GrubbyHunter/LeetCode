9e70d64e15dce603a60b70a6907581c35c15f4b9
karsashen <karsashen@tencent.com>
2022-01-11 19:18:14 +0800

    Dynamic programming/medium/1143.最长公共子序列.ts

--- Dynamic programming/medium/1143.最长公共子序列.ts
+++ Dynamic programming/medium/1143.最长公共子序列.ts
@@ -13,11 +13,20 @@ function longestCommonSubsequence(text1: string, text2: string): number {
     .fill(0)
     .map(() => new Array(arr2.length + 1).fill(0));
 
+  // 由于数组每次都是从第一个元素开始比较是否相同，所以i和j都从1开始方便计算
   for (let i = 1; i <= arr1.length; i++) {
     for (let j = 1; j <= arr2.length; j++) {
+      // 当前两个字符相等，取两个数组的上一个字符，二维数组前一行和前一列的值 + 1
       if (arr1[i - 1] === arr2[j - 1]) {
         dp[i][j] += dp[i - 1][j - 1] + 1;
       } else {
+        // 字符串不相等，但是当前数组的字符串可能与另一个数组的前一个字符串相等
+        // 例如 abca 和acab
+        // a b c a
+        // a c a b
+        // 最后两个字符（a,b不相等），但是第一个数组最后一个a与第二个数组倒数第二个a相等，
+        // 所以此时  dp[i][j] = dp[i][j-1] （倒数第二个a存的重复序列长度）
+        // 同样dp[i][j] 也可能等于 dp[i-][j]，两者取最大值
         dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
     }
