10359fa49966ec939f83e7c2e86150e84d6095a1
karsashen <karsashen@tencent.com>
2022-01-14 09:51:10 +0800

    Array/392.判断子序列-双指针法.ts

--- Array/392.判断子序列-双指针法.ts
+++ Array/392.判断子序列-双指针法.ts
@@ -1,14 +1,18 @@
 /*
- * @lc app=leetcode.cn id=35 lang=typescript
+ * @lc app=leetcode.cn id=392 lang=typescript
  *
- * [35] 搜索插入位置
+ * [392] 判断子序列
  */
+
+// @lc code=start
 function isSubsequence(s: string, t: string): boolean {
   let j = 0;
   let count = 0;
 
+  // 双指针法
   for (let i = 0; i < s.length; i++) {
     for (; j < t.length; j++) {
+      // 相等的话j++，同时跳出内层循环，比较下个数
       if (s[i] === t[j]) {
         count++;
         j++;
@@ -19,5 +23,4 @@ function isSubsequence(s: string, t: string): boolean {
 
   return count === s.length;
 }
-isSubsequence("aaaaaa", "bbaaaa");
 // @lc code=end
