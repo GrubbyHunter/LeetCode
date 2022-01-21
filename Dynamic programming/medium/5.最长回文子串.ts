021d5f225821a9c645e6c700cd0738df1100747e
karsashen <karsashen@tencent.com>
2022-01-21 09:22:05 +0800

    Dynamic programming/medium/5.最长回文子串.ts

---
+++ Dynamic programming/medium/5.最长回文子串.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=typescript
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+function longestPalindrome(s: string): string {
+  let dp = new Array(s.length)
+    .fill(false)
+    .map(() => new Array(s.length).fill(false));
+  let maxLength = 1;
+  let resultStr = s[0];
+  // 解法和 https://leetcode-cn.com/problems/palindromic-substrings/类似
+  // 多一步记录长度的操作
+  for (let i = s.length - 1; i >= 0; i--) {
+    for (let j = i; j < s.length; j++) {
+      if (s[i] !== s[j]) {
+        dp[i][j] = false;
+      } else {
+        if (j - i <= 1) {
+          dp[i][j] = true;
+        } else {
+          dp[i][j] = dp[i + 1][j - 1];
+        }
+      }
+
+      if (dp[i][j]) {
+        if (j - i + 1 > maxLength) {
+          maxLength = j - i + 1;
+          resultStr = s.substring(i, j + 1);
+        }
+      }
+    }
+  }
+
+  return resultStr;
+}
+// @lc code=end
