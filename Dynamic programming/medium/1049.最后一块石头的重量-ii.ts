e4113ecb139fb062108aed807f7bb13b97c23cda
karsashen <karsashen@tencent.com>
2022-01-22 17:04:34 +0800

    Dynamic programming/medium/1049.最后一块石头的重量-ii.ts

--- Dynamic programming/medium/1049.最后一块石头的重量-ii.ts
+++ Dynamic programming/medium/1049.最后一块石头的重量-ii.ts
@@ -1,8 +1,10 @@
 /*
- * @lc app=leetcode.cn id=35 lang=typescript
+ * @lc app=leetcode.cn id=1049 lang=typescript
  *
- * [35] 搜索插入位置
+ * [1049] 最后一块石头的重量 II
  */
+
+// @lc code=start
 function lastStoneWeightII(stones: number[]): number {
   let sum = 0
   let halfSum = 0
@@ -11,10 +13,6 @@ function lastStoneWeightII(stones: number[]): number {
     sum += stones[i]
   }
 
-  // 和的一半为正数，那么可以把数组分成和相等两部分，对着碰撞之后和为0
-  if (sum % 2 === 0) {
-    return 0
-  }
 
   // 一半的和作为背包的容量
   halfSum = Math.floor(sum / 2)
@@ -23,7 +21,7 @@ function lastStoneWeightII(stones: number[]): number {
   let dp = new Array(halfSum + 1).fill(0)
   // 解法类似 416.分割等和子集.ts
   for (let i = 0; i < stones.length; i++) {
-    // j > stones[i]表示当前容量足够放入i元素的值
+    // j >= stones[i]表示当前容量足够放入i元素的值
     for (let j = halfSum; j >= stones[i]; j--) {
       // 当前0到i个元素，存入容量为J的背包，和的最大值为dp[j]
       // 不放i时候的最大值 dp[j - stones[i]] 加上i的值stones[i]
@@ -31,7 +29,7 @@ function lastStoneWeightII(stones: number[]): number {
     }
   }
 
-  return sum - 2 * dp[dp.length]
+  return sum - 2 * dp[dp.length - 1]
 };
-lastStoneWeightII([2, 7, 4, 1, 8, 1]);
 // @lc code=end
+
