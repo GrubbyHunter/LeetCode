40cf1c19e9798c12b8bdd4f615705be7c6fea47f
karsashen <karsashen@tencent.com>
2022-02-26 18:16:02 +0800

    查找和排序/查找/矩阵/240.搜索二维矩阵-ii.ts

--- index.ts
+++ index.ts
@@ -1,83 +1,26 @@
 // @lc code=startfunction leastInterval(tasks: string[], n: number): number {
-function calcEquation(equations: string[][], values: number[], queries: string[][]): number[] {
-  let map = new Map()
-  let valueMap = new Map()
-  let result: any = []
-  let sum = 1.0
-  let hadFind: any = []
-  const findResult = (start: string, nextList: string[], end: string): number => {
-    if (hadFind.indexOf(start) > -1) {
-      return -1
-    }
+function searchMatrix(matrix: number[][], target: number): boolean {
+  let i = matrix.length - 1
 
-    if (!nextList) {
-      return -1
-    }
-    // 一招过的元素追加进数组，避免死循环
-    hadFind.push(start)
-
-    if (nextList.indexOf(end) > -1) {
-      sum *= valueMap.get(`${start}${end}`)
-      return sum
-    }
-
-    for (let i = 0; i < nextList.length; i++) {
-      if (!map.get(nextList[i])) {
-        continue
-      }
-
-      sum *= valueMap.get(`${start}${nextList[i]}`)
-      let next = findResult(nextList[i], map.get(nextList[i]), end)
-      if (next !== -1) {
-        return next
-      }
-
-      sum /= valueMap.get(`${start}${nextList[i]}`)
-    }
-    return -1
+  // 左下角第一个元素是当前列最大元素，也是当前行最小元素
+  // 所以先通过每一列第一个元素确定target在哪一行
+  while (matrix[i][0] > target && i >= 0) {
+    i--
   }
 
-  for (let i = 0; i < equations.length; i++) {
-    let current = equations[i]
-
-    // 记录下一个元素
-    if (!map.get(current[0])) {
-      map.set(current[0], [current[1]])
-    } else {
-      map.get(current[0]).push(current[1])
-    }
-
-    // 记录值
-    valueMap.set(`${current[0]}${current[1]}`, values[i])
+  // 越界，比matrix[0][0]还小
+  if (i < 0) {
+    return false
   }
 
-  for (let i = 0; i < queries.length; i++) {
-    let cur = queries[i]
-    if (cur[0] === cur[1]) {
-      result.push(1.0)
-      continue
+  // 找到哪一行后，遍历当前行找到是否属于此行
+  for (let j = 0; j < matrix[0].length; j++) {
+    if (matrix[i][j] === target) {
+      return true
     }
-
-    if (valueMap.get(`${cur[0]}${cur[1]}`)) {
-      result.push(valueMap.get(`${cur[0]}${cur[1]}`))
-      continue
-    }
-
-    if (valueMap.get(`${cur[1]}${cur[0]}`)) {
-      result.push(1 / valueMap.get(`${cur[1]}${cur[0]}`))
-      continue
-    }
-    // 重置sum
-    sum = 1.0
-    // 重置统计的已遍历数组
-    hadFind = []
-    result.push(findResult(cur[0], map.get(cur[0]), cur[1]))
   }
 
-  return result
+  // 当前行找不到，则不存在
+  return false
 };// @lc code=end
-calcEquation([["a", "b"], ["b", "c"]]
-  ,
-  [2.0, 3.0]
-  ,
-  [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]])
+searchMatrix([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 5)
