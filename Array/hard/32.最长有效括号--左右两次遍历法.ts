949d05ba84f356491341b77bdb59ed93420b8e9b
karsashen <karsashen@tencent.com>
2022-02-28 11:29:33 +0800

    Array/hard/32.最长有效括号--左右两次遍历法.ts

--- Array/hard/32.最长有效括号--左右两次遍历法.ts
+++ Array/hard/32.最长有效括号--左右两次遍历法.ts
@@ -1,10 +1,19 @@
-// @lc code=startfunction leastInterval(tasks: string[], n: number): number {
+/*
+ * @lc app=leetcode.cn id=32 lang=typescript
+ *
+ * [32] 最长有效括号
+ */
+
+// @lc code=start
 function longestValidParentheses(s: string): number {
   let left = 0
   let right = 0
   let max = 0
 
+  // 从左往右遍历统计连续有效括号个数
   for (let i = 0; i < s.length; i++) {
+    // 左右相等，当时当前为右括号，则说明左右有效括号被当前s[i]中断
+    // 例如 ()())(),第5个元素)中断连续有效的子数组
     if (left === right && s[i] == ")") {
       left = 0
       right = 0
@@ -18,12 +27,16 @@ function longestValidParentheses(s: string): number {
       right++
     }
 
+    // 左右相等，连续括号个数等于left+right => left * 2
     if (left === right) {
       max = Math.max(max, left * 2)
     }
   }
+
   left = 0
   right = 0
+
+  // 从右往左遍历统计连续有效括号个数
   for (let i = s.length - 1; i >= 0; i--) {
     if (left === right && s[i] == "(") {
       left = 0
@@ -42,7 +55,9 @@ function longestValidParentheses(s: string): number {
       max = Math.max(max, left * 2)
     }
   }
-
+  // 遍历两次是为了规避数据不全的情况
+  // 例如：()((())，这种第二个连续有效数组(())，会因为前面有个(
+  // left = 3，right = 2,无法获取到正确的结果4，从右往左则能找到这种结果
   return max
 };
-longestValidParentheses(")()())")
+// @lc code=end
