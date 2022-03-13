1c0ae5155d2ba02abc21ff44b92a8d4b62ba7995
karsashen <karsashen@tencent.com>
2022-03-13 14:32:18 +0800

    分治法/middle/395.至少有-k-个重复字符的最长子串.ts

--- 分治法/middle/395.至少有-k-个重复字符的最长子串.ts
+++ 分治法/middle/395.至少有-k-个重复字符的最长子串.ts
@@ -1,4 +1,9 @@
-// @lc code=startfunction leastInterval(tasks: string[], n: number): number {
+/*
+ * @lc app=leetcode.cn id=395 lang=typescript
+ *
+ * [395] 至少有 K 个重复字符的最长子串
+ */
+
 // @lc code=start
 function longestSubstring(s: string, k: number): number {
   let max = 0
@@ -7,8 +12,10 @@ function longestSubstring(s: string, k: number): number {
     return s.length
   }
 
+  // left: 左边区间下标 right：右边区间下标
   const findMaxLength = (left: number, right: number) => {
     const map: any = new Map()
+    // 数组越界，或者区间不足k个元素，返回
     if (left >= right || right - left + 1 < k) {
       return
     }
@@ -25,28 +32,39 @@ function longestSubstring(s: string, k: number): number {
     }
 
     let indexList: any = []
+    // 统计出现小于k的字母，并保存他们的下标
     for (let [key, list] of map) {
       if (list.length < k) {
         indexList = indexList.concat(list)
       }
     }
 
+    // 如果不存在出现小于k次的字母
+    // 则此区间满足条件，进行长度统计
     if (indexList.length === 0) {
       max = Math.max(max, right - left + 1)
       return
     }
 
+    // 数组排序
     indexList.sort((a: number, b: number) => a - b)
 
     let start: number = left
+    // 从最左边开始，用下标拆分字符串，例如b的下标是5，b出现了2次，但是k等于3
+    // 则b肯定不在满足条件范围的区间，用b将数组查分，继续比较
     for (let i = 0; i < indexList.length; i++) {
+      // start到当前indexList[i]区间不足k个元素，也不满足条件
+      // 不继续查找
       if (indexList[i] - start + 1 >= k) {
+        // 拆分完，继续查找下一个子区间
         findMaxLength(start, indexList[i] - 1)
       }
 
+      // 区见左边下标变为当前右边下标+1
       start = indexList[i] + 1
     }
 
+    // 最后一个分割点(start)到数组结尾(right)依然超过k个元素，则start-right区间也需要继续统计
     if (right - start + 1 >= k) {
       findMaxLength(start, right)
     }
@@ -56,4 +74,5 @@ function longestSubstring(s: string, k: number): number {
 
   return max
 };
-longestSubstring("bbaaacbd", 3)
+// @lc code=end
+
