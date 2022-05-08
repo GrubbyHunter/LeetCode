37fed962de145c413849aaf1c8ff3336e6db5fe6
karsashen <karsashen@tencent.com>
2022-05-08 21:55:40 +0800

    index.ts

--- 25.k-个一组翻转链表.ts
+++ 25.k-个一组翻转链表.ts
@@ -1,18 +1,26 @@
-// @lc code=startfunction leastInterval(tasks: string[], n: number): number {
+/*
+ * @lc app=leetcode.cn id=25 lang=typescript
+ *
+ * [25] K 个一组翻转链表
+ */
+
 // @lc code=start
-class ListNode {
-  val: number
-  next: ListNode | null
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = (val === undefined ? 0 : val)
-    this.next = (next === undefined ? null : next)
-  }
-}
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
 function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
   let length = 1
-  let result: any = head
-  let temp: any = head
+  let result = head
+  let temp = head
 
   // 先统计链表长度
   while (temp.next) {
@@ -23,19 +31,19 @@ function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
   // 需要反转的总长度
   let size = Math.floor(length / k) * k
   let count = 0
-  let pre: any = new ListNode(0, head)
+  let pre = new ListNode(null, head)
 
   while (size === 0) {
     size--
 
     // 头插法
-    let curNext: ListNode = head.next!
-    // // // 移除翻转的后裔个节点
-    head.next = curNext.next
+    let temp = head.next
+    // 移除翻转的后裔个节点
+    head.next = temp.next
     //后一个节点移到当前head，也就是前一个节点前面
-    curNext.next = head
+    temp.next = head
     // 头部接上移过来的前一个节点
-    pre.next = curNext
+    pre.next = temp
 
     count++
 
@@ -43,15 +51,12 @@ function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
     // pre 为当前翻转的最后一个节点，head为下一个新的节点
     if (count === k) {
       pre = head
-      // head = head.next
+      head = head.next
 
       count = 0
     }
   }
   return result
 };
-reverseKGroup(new ListNode(1,
-  new ListNode(2,
-    new ListNode(3,
-      new ListNode(4,
-        new ListNode(5))))), 2)
+// @lc code=end
+
