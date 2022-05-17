2e982856d800959a251a71da66674e2d9edf4d97
karsashen <karsashen@tencent.com>
2022-05-17 10:58:13 +0800

    Dynamic programming/hard/188.买卖股票的最佳时机-iv.ts

--- Dynamic programming/hard/188.买卖股票的最佳时机-iv.ts
+++ Dynamic programming/hard/188.买卖股票的最佳时机-iv.ts
@@ -6,35 +6,32 @@
 
 // @lc code=start
 function maxProfit(k: number, prices: number[]): number {
+  // 初始化dp数组
   let dp = new Array(prices.length).fill(0).map(() => new Array(2 * k + 1).fill(0))
 
   if (prices.length === 0) {
     return 0
   }
 
+  // 初始化dp数组第一天的各种状态
   for (let i = 0; i < 2 * k + 1; i++) {
-    // 初始化第一天的持有现金，下标0为不持有的情况，1,3,5下标分别表示第1,2,3次买入的情况
-    // 买入持有现金就是减去第一天的价格，其他元素生成数组时候就是0，不需要初始化
-    if (i % 2 !== 0) {
-      dp[0][i] = -prices[0]
+    if (i % 2 === 0) {
+      dp[0][i] = 0 // 第0天，第1次卖出，第2次卖出，持有金额都是0
+    } else {
+      dp[0][i] = -prices[0] // 第1次买入，第2次买入，持有金额都是负的第一天的价格，以此类推
     }
   }
 
   for (let i = 1; i < prices.length; i++) {
-    dp[i][0] = dp[i - 1][0]
+    dp[i][0] = dp[i - 1][0] // 0为第i天未买入股票时候的持有金额，实际上他一直就是0
 
-    for (let j = 1; j < 2 * k + 1; j++) {
-      let prePrice = dp[i][j - 1]
-      // 第j%2次卖出
-      if (j % 2 === 0) {
-        // 卖出 + 当前天价格
-        prePrice += prices[i]
-      } else {
-        // 第j%2次买入，买入减去当前天价格
-        prePrice -= prices[i]
-      }
-      // 和维持前一天状态的情况比较，取较大值
-      dp[i][j] = Math.max(dp[i - 1][j], prePrice)
+    for (let j = 1; j < 2 * k + 1; j += 2) {
+      // 第i天第j次买入时候之后的持有现金 = 第0到i-1天买入时候持有现金 or 第i天买入（即上一个状态持有现金 - 当天价格）
+      // 取最大值
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1] - prices[i])
+      // 第i天第j次卖出时候之后的持有现金 = 第0到i-1天卖出时候持有现金 or 第i天卖出（即上一个状态持有现金 + 当天价格）
+      // 取最大值
+      dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i][j] + prices[i])
     }
   }
 
