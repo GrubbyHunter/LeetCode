bd01e8d74ef5f359fccc30b757fd4cec5933e224
karsashen <karsashen@tencent.com>
2022-05-23 09:56:45 +0800

    Dynamic programming/medium/213.打家劫舍-ii.ts

--- Dynamic programming/medium/213.打家劫舍-ii.ts
+++ Dynamic programming/medium/213.打家劫舍-ii.ts
@@ -6,35 +6,40 @@
 
 // @lc code=start
 function rob(nums: number[]): number {
-  if (nums.length === 1) {
+  let len = nums.length
+  if (len === 1) {
     return nums[0]
   }
+
+  if (len === 2) {
+    return Math.max(nums[0], nums[1])
+  }
+
   // dp[i]表示偷到第i家能偷到的最大值
-  let dp = new Array(nums.length).fill(0)
+  let dp = new Array(len).fill(0)
   // 记录最大值
   let max = 0
 
-  // 这里分两种情况
-  // 第一种，从第一家开始偷，那么由于是环，最后一家不能偷
+  // 分为偷最后一家和不偷最后一家两种情况
+
+  // 一、不偷最后一家，那么从第1家开始计算
   dp[0] = nums[0]
-  dp[1] = Math.max(nums[1], nums[0])
-  // 从第3家开始初始化求值，由于最后一家不能偷，所以遍历到倒数第二家
-  for (let i = 2; i < nums.length - 1; i++) {
-    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
+  dp[1] = Math.max(dp[0], dp[1])
+  // 遍历到倒数第二家
+  for (let i = 2; i < len - 1; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
   }
-  // 统计偷到的最大值
-  max = dp[nums.length - 2]
 
+  max = dp[len - 2]
 
-  // 第二种情况，需要偷最后一家，那么第一家不能偷，重新初始化数组
-  dp = new Array(nums.length).fill(0)
-  // 第一家不能偷， == 0
-  dp[0] = 0
-  dp[1] = nums[1] // 第二家可以偷，第1家是0，所以不用比较，第2家是nums[i]
-
-  for (let i = 2; i < nums.length; i++) {
-    // 遍历求值时候统计到最后一家
-    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
+  // 二、偷最后一家，那么第一家不能偷，那么从第2家开始计算
+  // 重新初始化dp
+  dp = new Array(len).fill(0)
+  dp[0] = 0 // 不偷第1家，所以是0
+  dp[1] = nums[1]
+  // 一直遍历到结尾
+  for (let i = 2; i < len; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
   }
 
   // 比较两种情况，获取比较大的那个值
