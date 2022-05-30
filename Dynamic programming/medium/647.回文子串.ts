3e90232514917f51fac09eb29d15d2b586b9c754
karsashen <karsashen@tencent.com>
2022-05-30 09:09:41 +0800

    Dynamic programming/medium/647.回文子串.ts

--- Dynamic programming/medium/647.回文子串.ts
+++ Dynamic programming/medium/647.回文子串.ts
@@ -25,18 +25,18 @@ function countSubstrings(s: string): number {
     for (let j = i; j < s.length; j++) {
       // 第一种情况
       if (s[i] !== s[j]) {
-        dp[i][j] = false;
+        dp[i][j] = false
       } else {
         // 2.1种情况，这里如果i=j或者ij只差小于等于1
         // 也就是当前字符串只有1到2个字符，直接判断即可
         if (j - i <= 1) {
-          dp[i][j] = true;
+          dp[i][j] = true
         } else {
           // 2.2种情况
           dp[i][j] = dp[i + 1][j - 1];
         }
-      }
 
+      }
       // 最后dp记录当前i到j的子串是否是回文的状态，是的话总数+1
       // 此题跟暴力解法前面是类似的，不过后面是使用dp二维数组记录之前的子串是否是回文状态
       // 再扩大i和j的范围之后，就可以复用之前的状态，相当于是一种空间换时间的做法
