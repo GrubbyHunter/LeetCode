1aa13a7f1bb4936862a26bf2d37fc76203d27288
karsashen <karsashen@tencent.com>
2022-06-22 22:51:43 +0800

    List/328.奇偶链表.ts

--- List/328.奇偶链表.ts
+++ List/328.奇偶链表.ts
@@ -29,12 +29,12 @@ function oddEvenList(head: ListNode | null): ListNode | null {
 
   // 只需要用偶数指针和他的next作为结束的判断条件
   while (double && double.next) {
-    //第一个数的指针指向第三个数
+    //第一个数的指针指向第三个数，相当于删除第二个数
     single.next = single.next.next
     // 当前指针为第三个数，将他赋值给第一个数
     single = single.next
 
-    //第二个数的指针指向第四个数
+    //第二个数的指针指向第四个数，相当于删除第3个数
     double.next = double.next.next
     // 当前指针为第四个数，将他赋值给第二个数
     double = double.next
@@ -42,7 +42,6 @@ function oddEvenList(head: ListNode | null): ListNode | null {
 
   // 奇数指针结尾指向偶数指针的开头
   single.next = doublePointer
-
   // 返回奇数指针
   return singlePointer
 };
