fcba8d88bdb2c30addda4b09245003748c238232
karsashen <karsashen@tencent.com>
2022-06-27 11:29:19 +0800

    滑动窗口/middle/239.滑动窗口最大值.ts

--- 滑动窗口/middle/239.滑动窗口最大值.ts
+++ 滑动窗口/middle/239.滑动窗口最大值.ts
@@ -12,17 +12,16 @@ function maxSlidingWindow(nums: number[], k: number): number[] {
   let left = 1, right = k - 1
   let i = 0
 
+
   // 寻找元素在降序队列中的位置，并存入
-  // 说明下，这里如果当前值超过队列最大值，会将队列前面的所有值移除
-  // 因为在这个最大值移除滑动窗口前，他会一直是这个滑动窗口的最大值
-  // 前面的值可以忽略不计
   // 这里实际上更适合用升序，吧最大值放在模拟数组的尾部
   // 这样每次删除只处理数组的尾部元素就行
   // 为了模拟栈的行为方便理解，才放在头部处理
   let findIndex = (num: number): void => {
-    // 大于队列最大值(队列的第一个)
+    // 说明下，这里如果当前值超过队列最大值，会将队列前面的所有值移除
+    // 因为在这个最大值移除滑动窗口前，他会一直是这个滑动窗口的最大值
+    // 前面的值可以忽略不计
     if (num > queue[0]) {
-      // 清空队列，重新把最大值放进去
       queue = [num]
     } else {
       let last = queue[queue.length - 1]
@@ -46,20 +45,19 @@ function maxSlidingWindow(nums: number[], k: number): number[] {
   result.push(queue[0])
 
   while (right < nums.length - 1) {
-    let max = queue[0]
-
-    // left-1，为上一次移出范围的元素
-    // 最大值等于左侧需要出滑动范围的元素的话
-    if (max === nums[left - 1]) {
-      // 最大值出队列
+    // 左边将要移除的元素大于队列中最大元素
+    // 这里nums[left - 1]不可能出现在queue[0]的后面
+    // 因为findIndex中先加入，但是较小的元素会推出队列
+    if (nums[left - 1] === queue[0]) {
+      // 移除队列中最大元素
       queue.shift()
     }
-    // right+1为下一个要移进来的元素
-    // 找到右侧进入滑动范围的元素在队列中的位置
+
+    // 右侧将要移动进来的元素，找到对应的位置
     findIndex(nums[right + 1])
-    // 将队列第一个元素（也就是滑动窗口的最大值）放入结果
+
+    // 先将第一个滑动窗口的最大值放入结果
     result.push(queue[0])
-    // 比较完之后进行下一次遍历
     left++
     right++
   }
