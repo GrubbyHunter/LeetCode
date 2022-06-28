f6dc38b2afa8a00b898a0abeb09f62780a6f99ad
karsashen <karsashen@tencent.com>
2022-06-28 09:51:13 +0800

    滑动窗口/middle/424.替换后的最长重复字符.ts

--- 滑动窗口/middle/424.替换后的最长重复字符.ts
+++ 滑动窗口/middle/424.替换后的最长重复字符.ts
@@ -12,28 +12,30 @@ function characterReplacement(s: string, k: number): number {
   // 数组下标记录字母ascii code 位置，数组值记录字母出现次数
   let countArr = new Array(26).fill(0)
   let left = 0, right = 0
-
+  // 这里有点贪心算法的思维，left到right之间出现最多的字母出现了maxCnt次
+  // 我们尽可能利用k来翻转left到right之间的其他字母，这样尽可能的让k充分利用
+  // 例如：“AABAB”,k=2,这时候最多的是A，出现了3次，我们肯定是去翻转B，来让A达到更多
   for (; right < s.length; right++) {
-    // 由于是大写字母，所以生成下标这里是-65，不是-97
-    countArr[s.charCodeAt(right) - 65]++ // 计算出现次数
-    // 当前left到right直接出现次数最大的字母
-    maxCnt = Math.max(maxCnt, countArr[s.charCodeAt(right) - 65])
+    // 这里字母都是大写，所以减去 65 而不是97
+    // 当前字母的数量 + 1
+    countArr[s[right].charCodeAt(0) - 65]++
+    // 获取left - right 区间出现字母最多的次数maxCnt
+    maxCnt = Math.max(maxCnt, countArr[s[right].charCodeAt(0) - 65])
 
-    // 这里有点贪心算法的思维，left到right之间出现最多的字母出现了maxCnt次
-    // 我们尽可能利用k来翻转left到right之间的其他字母，这样尽可能的让k充分利用
-    // 例如：“AABAB”,k=2,这时候最多的是A，出现了3次，我们肯定是去翻转B，来让A达到更多
-
-    // k + 最多次数的字母，不足区间长度，说明k次不足已替换剩下的字母
-    while (right - left + 1 > k + maxCnt) {
-
-      // 移动完左边区间里移出元素的个数需要-1
-      countArr[s.charCodeAt(left) - 65]--
-      // 滑动窗口过大，需要移动左边
+    // 当前滑动窗口区间为[left,right]，里面有right - left + 1 个元素
+    // 如果出现最多的元素maxCnt 加上翻转次数k，小于这个区间元素个数
+    // 那么需要左边窗口缩小，left++
+    // 实际上只要right - left + 1 等于 maxCnt + k，那么条件成立
+    // 当前区间元素个数 = 最多的元素个数maxCnt + k 需要翻转的元素个数
+    while (right - left + 1 > maxCnt + k) {
+      // left移动时，将当期按移除元素的数量 - 1
+      countArr[s[left].charCodeAt(0) - 65]--
       left++
     }
-
-    // 满足条件
-    // k + 最多次数的字母，大于等于区间长度，说明k次足以替换剩下的字母
+    // 统计最大值，这里left移动时候maxCnt不需要-1
+    // 因为区间大小跟maxCnt + k相等时，result的值才成立
+    // 如果区间缩小了（left++），这时候计算出来的result肯定会小于之前的区间最大值
+    // result的值会被下面这个max覆盖
     result = Math.max(right - left + 1, result)
   }
 
