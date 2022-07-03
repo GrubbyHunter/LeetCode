65e03a0c1474cc47fe9216fbde634757fe244134
karsashen <karsashen@tencent.com>
2022-07-03 14:50:25 +0800

    滑动窗口/middle/567.字符串的排列--滑动窗口解法.ts

--- 滑动窗口/middle/567.字符串的排列--滑动窗口解法.ts
+++ 滑动窗口/middle/567.字符串的排列--滑动窗口解法.ts
@@ -12,11 +12,12 @@ function checkInclusion(s1: string, s2: string): boolean {
 
   // 判断两个数组内元素是否相等
   const isSameArr = (arr1: number[], arr2: number[]): boolean => {
-    if (arr1.length !== arr2.length) {
-      return false;
-    }
-
     for (let i = 0; i < arr1.length; i++) {
+      // 没出现过的字母不用比较
+      if (arr1[i] === 0) {
+        continue
+      }
+
       if (arr1[i] !== arr2[i]) {
         return false;
       }
@@ -34,18 +35,17 @@ function checkInclusion(s1: string, s2: string): boolean {
     sourceArr[s2.charCodeAt(i) - 97]++;
   }
 
-  // 区间遍历
+  // 区间遍历，上面已经遍历过一个s1的长度，所以下面只需要遍历s2-s1的长度
   for (let i = 0; i <= s2.length - s1.length; i++) {
     // 区间内字母出现次数完全一致，说明能找到target的排列之一
     if (isSameArr(targetArr, sourceArr)) {
       return true;
     }
-
-    // 下次比较之前
-    // 左边移除-1
-    sourceArr[s2.charCodeAt(i) - 97]--;
-    // 右边进入，+1
-    sourceArr[s2.charCodeAt(i + s1.length) - 97]++;
+    //在 s2中维持一个s1长度的窗口，判断窗口内字母出现的次数是否相同
+    // 移出窗口
+    sourceArr[s2.charCodeAt(i) - 97]--
+    // 移入窗口
+    sourceArr[s2.charCodeAt(i + s1.length) - 97]++
   }
 
   return false;
