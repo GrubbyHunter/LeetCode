b0f8f815d1c934b18d9fce21440a73e7a086d71a
karsashen <karsashen@tencent.com>
2022-07-17 21:34:25 +0800

    index.ts

--- index.ts
+++ index.ts
@@ -1,44 +1,44 @@
 // @lc code=startfunction leastInterval(tasks: string[], n: number): number {
 // @lc code=start
-function letterCombinations(digits: string): string[] {
-  const result: any = []
-  const phoneObj: any = {
-    "2": ["a", "b", "c"],
-    "3": ["d", "e", "f"],
-    "4": ["g", "h", "i"],
-    "5": ["j", "k", "l"],
-    "6": ["m", "n", "o"],
-    "7": ["p", "q", "r", "s"],
-    "8": ["t", "u", "v"],
-    "9": ["w", "x", "y", "z"]
-  }
-  const digitsArr = digits.split("")
-  // n为结果数组中每个元素的长度，输入2个数字，name每个元素长度为2
-  const n = digitsArr.length
-
-  if (n === 0) {
-    return []
+function restoreIpAddresses(s: string): string[] {
+  const result: string[] = []
+  let sArr = s.split("")
+  const length = sArr.length
+  if (length === 0) {
+    return result
   }
 
-  const backTracking = (arrStr: string[], start: number): void => {
-    // 每个按键选一个字母，长度满足条件
-    if (arrStr.length === n) {
-      result.push(arrStr.join(""))
+  const backTracking = (arr: string[], start: number) => {
+    // 超过ip长度
+    if (arr.length > length) {
       return
     }
 
-    for (let i = start; i < digitsArr.length; i++) {
-      const strArr = phoneObj[digitsArr[i]]
+    if (arr.length === 4 && start === sArr.length) {
+      result.push(arr.join("."))
+      return
+    }
 
-      for (let j = 0; j < strArr.length; j++) {
-        arrStr.push(strArr[j])
-        backTracking(arrStr, start + 1)
-        arrStr.pop()
+    for (let i = start; i < length; i++) {
+      let str = sArr.slice(start, i + 1)
+
+      if (str.length > 3) {
+        break
       }
+
+      let numberStr = str.join("")
+      if (str.length === 3) {
+        if (parseInt(numberStr, 10) > 255) {
+          break
+        }
+      }
+      arr.push(numberStr)
+      backTracking(arr, i + 1)
+      arr.pop()
     }
   }
 
   backTracking([], 0)
   return result
 };
-letterCombinations("23")
+restoreIpAddresses("25525511135")
