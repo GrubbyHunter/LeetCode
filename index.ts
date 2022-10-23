a5149a03e6baa30bc1ac6a7472bc3f4c112a0977
GrubbyHunter <GrubbyHunter@gmail.com>
2022-10-23 14:06:51 +0800

    List/middle/2.两数相加.ts

--- index.ts
+++ index.ts
@@ -22,48 +22,40 @@ class ListNode {
     this.next = (next === undefined ? null : next)
   }
 }
-function nextLargerNodes(head: ListNode | null): number[] {
-  if (!head) {
-    return []
-  }
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  let addOne = false
+  let result:ListNode = new ListNode(0)
+  let cur = result
+  while(l1 && l2){
+    let value = l1.val+l2.val + (addOne? 1:0)
 
-  if (!head.next) {
-    return [0]
-  }
-
-  // 链表转数组，临时统计数据
-  let tempList: number[] = []
-  while (head) {
-    tempList.push(head.val)
-    head = head.next
-  }
-
-  
-  // 记录结果
-  let result: number[] = new Array(tempList.length)
-  // 使用数组模拟单调栈
-  let stack: number[] = []
-    // 从右往左遍历
-  for (let i = tempList.length - 1; i >= 0; i--) {
-    // 移除掉单调栈中小于当前元素的数字
-    // 因为单调栈是从右往左放入进去的，所以放进去的元素一定在当前元素的右边
-    while(stack.length > 0 && stack[stack.length-1] <= tempList[i]){
-      stack.pop()
-    }
-    
-    // 栈中没有比当前元素大的元素，当前位置记录为0
-    if(stack.length === 0){
-      result[i] = 0
+    if(value >= 10){
+      value =value % 10
+      addOne = true
     }else {
-      // 当前元素比栈顶元素小，记录栈顶元素为当前元素右侧第一个比他大的元素
-      result[i] = stack[stack.length-1]
+      addOne = false
     }
 
-    // 需要将当前元素推入栈顶
-    stack.push(tempList[i])
+    cur.next = new ListNode(value)
+    cur = cur.next
+
+    l1 = l1.next
+    l2 = l2.next
   }
 
-  return result
+  // 两个指针长度相同，都为空，直接返回
+  if(!l1 && !l2){
+    return result.next
+  }
+
+  // 需要拼接长的指针剩余部分
+  if(l1){
+    cur.next = l1
+  }else {
+    cur.next = l2
+  }
+  
+  return result.next
 };
 
 
@@ -71,5 +63,7 @@ function nextLargerNodes(head: ListNode | null): number[] {
 
 
 
-
-nextLargerNodes(new ListNode(2, new ListNode(7, new ListNode(4, new ListNode(3,new ListNode(5))))))
+addTwoNumbers(
+  new ListNode(9, new ListNode(9, new ListNode(9))),
+  new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+)
