50d1c40f2a12297e05b44abc45a96707430bcdb3
GrubbyHunter <GrubbyHunter@gmail.com>
2022-10-24 23:08:39 +0800

    138.复制带随机指针的链表.ts

---
+++ 138.复制带随机指针的链表.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=138 lang=typescript
+ *
+ * [138] 复制带随机指针的链表
+ */
+
+// @lc code=start
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     next: Node | null
+ *     random: Node | null
+ *     constructor(val?: number, next?: Node, random?: Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *         this.random = (random===undefined ? null : random)
+ *     }
+ * }
+ */
+
+function copyRandomList(head: Node | null): Node | null {
+    
+};
+// @lc code=end
+
