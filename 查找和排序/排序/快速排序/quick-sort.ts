6a3728e817ea603333ffb91e565a15b05ac1879a
grubbyhunter <grubbyhunter@gmail.com>
2022-11-05 20:46:43 +0800

    add quick sort

--- 查找和排序/排序/快速排序/quick-sort.ts
+++ 查找和排序/排序/快速排序/quick-sort.ts
@@ -1,16 +1,3 @@
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
 /**
  Do not return anything, modify head in-place instead.
  */
